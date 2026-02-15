27852e772ce11207ca6cf4b7ff1e6d7e44af9dd3
Helani Ambalangodage <helaniuambalangodage@gmail.com>
2026-02-15 16:45:20 +0530

    change startup page again

--- frontend/src/pages/Auth/StartingPage.jsx
+++ frontend/src/pages/Auth/StartingPage.jsx
@@ -17,9 +17,9 @@ export default function GetStarted() {
 
         {/* Right side: Contact Number */}
         <div>
-          <p className="text-white font-semibold">Helani - 📞
+          <p className="text-white font-semibold">Contact us - 📞
             <a href="tel:+94771234567" className="text-white font-semibold hover:underline">
-              +94 77 123 4567999
+              +94 77 123 4567
             </a>
           </p>
         </div>
